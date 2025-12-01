8d549f6e3b566507ab42e0d9e3b395d2826d7e8f
Hoàng Đức Toàn <109254834+Akimitsujiro@users.noreply.github.com>
2025-12-01 23:29:11 +0700

    Implement TextDetector class for image text detection
    Added a TextDetector class for detecting text in images using a comic text detection model.

--- modules/detection.py
+++ modules/detection.py
@@ -1,1 +1,25 @@
 
+# modules/detection.py
+from comic_text_detector.inference import TextDetector as ComicTextDetector
+import numpy as np
+from PIL import Image
+
+class TextDetector:
+    def __init__(self, model_path=None):
+        self.model = ComicTextDetector(model_path=model_path, input_size=1024, device='cuda')
+        print("✅ Text Detector loaded!")
+
+    def detect(self, image_path):
+        """
+        Trả về list các bounding box: [[x_min, y_min, x_max, y_max], ...]
+        """
+        detection_results = self.model(image_path)
+        
+        bboxes = detection_results[1] 
+        
+        final_bboxes = []
+        for box in bboxes:
+             x, y, w, h = box
+             final_bboxes.append([int(x), int(y), int(x+w), int(y+h)])
+             
+        return final_bboxes
