6f6a169384cfffe10fe175b78dced5bd99c17876
Hoàng Đức Toàn <109254834+Akimitsujiro@users.noreply.github.com>
2025-12-01 23:30:22 +0700

    Add MangaOCR_Wrapper class for OCR functionality

--- modules/ocr.py
+++ modules/ocr.py
@@ -1,1 +1,17 @@
+from manga_ocr import MangaOcr
+from PIL import Image
 
+class MangaOCR_Wrapper:
+    def __init__(self):
+        self.ocr = MangaOcr()
+        print("✅ Manga OCR loaded!")
+
+    def run(self, image, bbox):
+        """
+        image: PIL Image object gốc
+        bbox: [x_min, y_min, x_max, y_max]
+        """
+        cropped_image = image.crop(bbox)
+        
+        text = self.ocr(cropped_image)
+        return text
